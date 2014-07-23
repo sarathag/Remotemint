822b5a151e2392d353ada76095537a71f923871f
Sarath <sarath.ag@compassitesinc>
2014-07-23 18:11:55 +0530

    third commit added mymint1 java class

---
+++ Mynts/src/com/mynts/sales/Mymint1.java
@@ -0,0 +1,10 @@
+package com.mynts.sales;
+
+public class Mymint1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first output fir Mymint1 class");
+	}
+
+}
