0ddfbb42a9e014882044d1445cfc18e528a9af38
Sarath <sarath.ag@compassitesinc>
2014-07-23 18:02:32 +0530

    First commit

---
+++ Mynts/src/com/mynts/sales/myMint.java
@@ -0,0 +1,12 @@
+package com.mynts.sales;
+
+public class myMint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("first output");
+ 
+	}
+
+}
